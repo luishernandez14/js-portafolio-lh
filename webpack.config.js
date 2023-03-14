93b34fa76ad55b0a72612c0aef8768ce84d549f2
luishernandez14 <alejolahr14@gmail.com>
2023-03-14 17:32:37 -0500

    Images moved to distribution folder using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,6 +20,12 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'],
       }
     ]
   },
@@ -26,6 +34,15 @@ module.exports = {
         inject: true,
         template: './public/index.html',
         filename: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
     })
   ]
 }
